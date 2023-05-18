aa299ba78613846f689e99c73f4461aba26d4c59
SayhanA <sayhanahmed05@gmail.com>
2023-05-18 10:55:38 +0600

    display section added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -13,7 +13,11 @@ const Home = () => {
         <div className=' w-full'>
             <Banner />
             <h3 className='lg:text-3xl text-center font-bold py-5 pt-10'>Top Rated Selling Cars</h3>
-            
+            <div className='grid lg:grid-cols-3 p-32 pt-10 gap-10 mx-auto'>
+                {
+                    loader.slice(0, 6).map(car => <Display key={car._id} car={car}></Display>)
+                }
+            </div>
         </div>
     );
 };
